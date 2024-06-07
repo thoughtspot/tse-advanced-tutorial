6719aed090b7cf377a1a0f4b1a19cfd03ea5b576
Bill Back <32280587+billdback-ts@users.noreply.github.com>
2024-06-07 13:44:56 -0400

    Added support for passcode for the token server.

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -4,6 +4,7 @@ export const constants = {
   tokenServer: 'https://tse-auth-token-server-example.vercel.app',  // best not to use the final /
   tsURL: 'https://training.thoughtspot.cloud',
   developerDocs: 'https://developers.thoughtspot.com/docs',
+  passcode: 'passcode4TokenService', // must match what the token server expects.
 }
 
 export const cssFiles = {
--- src/lib/util.js
+++ src/lib/util.js
@@ -4,7 +4,7 @@ import {constants} from "$lib/constants.js";
  * Gets a token for the given user.
  */
 export const getAuthToken = async (user) => {
-  const endpoint = `${constants.tokenServer}/token?username=${user}`;
+  const endpoint = `${constants.tokenServer}/token?username=${user}$passcode=${constants.passcode}`;
   console.log("token endpoint: " + endpoint);
 
   const response = await fetch(endpoint);
